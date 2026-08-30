6dafc82f1b737930a1b509a9bb2ad2a1182cef31
Tanzilalimam <gittanzilal@gmail.com>
2026-08-30 21:04:23 +0600

    updt-aug30

--- src/components/layouts/copyright/Copyright.jsx
+++ src/components/layouts/copyright/Copyright.jsx
@@ -4,7 +4,7 @@ const CopyRight = function(){
     return(
         <section className="py-5 bg-[#161c2d] border-t-2 border-t-[#ffffff1a]">
             <div className="container">
-                <div className="flex justify-around items-center">
+                <div className="flex justify-between items-center">
                     <p className="text-white text-[16px] capitalize">&copy; copyright 2026 - uclean by designasia</p>
                     <div className="flex gap-5">
                     <a href="#" className="text-white text-[16px] capitalize">terms & conditions</a>
